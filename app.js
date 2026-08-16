466afc8d8c8c653809feaffb1f7b018d0acd73a7
Ali <ali.kamel5958@gmail.com>
2026-08-16 21:03:08 +0300

    questions doesn't repeate now, level indicator, start only clicked once, new exit button. we're reaching the mvp.. only more questions needed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ----- Variables ----- //
-let time = 20
+let time = 10
 let currentTime = time
 let levelIndex = 0
 let level = 0
@@ -62,6 +62,8 @@ const questions = [
 ]
 // --- cache --- //
 const StartButtonElement = document.querySelector('#start-button')
+const ExitButtonElement = document.querySelector('#exit-button')
+const levelIndicatorElement = document.querySelector('#levelIndicator')
 const timerElement = document.querySelector('#timer')
 const questionWordsElement = document.querySelector('#questionWords')
 const playerInputElement = document.querySelector('#playerInput')
@@ -92,33 +94,35 @@ function startFunc() {
     if (!active) {
         currentTime = time
         timeInterval = setInterval(currentTimeFunc, 1000);
+        randomize()
         leveling()
         active = true
     }
 }
 function leveling() {
     console.log("LEVEL " + level)
+    levelIndicatorElement.textContent = "LEVEL " + level
     //let RandomNum = getRandomNum(0, questions.length); (old way, not used now)
-    randomize()
+    console.log("level index" + levelIndex)
     if (level >= 0 && level <= 11) {
-        questions.words.sort(() => Math.random() - .5)
+        questions[levelIndex].words.sort(() => Math.random() - .5)
         console.log(questions[levelIndex].words.join(" | "))
         questionWordsElement.textContent = questions[levelIndex].words.join(" | ")
     } else if (level >= 11 && level <= 21) {
-        questions.words.sort(() => Math.random() - .5)
-        questions[levelIndex].words.pop()
-        console.log(questions[levelIndex].words.join(" | "))
-        questionWordsElement.textContent = questions[levelIndex].words.join(" | ")
+        questions[levelIndex].words.sort(() => Math.random() - .5)
+        console.log(questions[levelIndex].words[0] + " | " + questions[levelIndex].words[1])
+        questionWordsElement.textContent = questions[levelIndex].words[0] + " | " + questions[levelIndex].words[1]
     } else if (level >= 21) {
-        questions.words.sort(() => Math.random() - .5)
-        questions[levelIndex].words.pop()
-        questions[levelIndex].words.pop()
+        questions[levelIndex].words.sort(() => Math.random() - .5)
         console.log(questions[levelIndex].words[0])
-        questionWordsElement.textContent = questions[levelIndex].wordOne
+        questionWordsElement.textContent = questions[levelIndex].words[0]
     }
 }
 function resetLevel() {
+    clearInterval(timeInterval);
+    timerElement.textContent = "10"
     level = 0
+    levelIndex = 0
     currentTime = time
     active = false
     questionWordsElement.textContent = "click START to start the game"
@@ -127,22 +131,26 @@ function checkEnter(event) {
     if (event.code == 'Enter') {
         console.log("You just pressed Enter")
         console.log(playerInputElement.value)
-        console.log(questions[levelIndex].Root)
-        if (playerInputElement.value === questions[levelIndex].Root) {
+        if (playerInputElement.value === questions[levelIndex].Root && active == true) {
 
             console.log("Your Answer is True")
-            currentTime += 5
+            currentTime += 2
             level = level + 1
+            levelIndex++
             leveling()
         }
-        else {
+        else if (playerInputElement.value != questions[levelIndex].Root && active == true) {
             console.log("Your Answer is Wrong")
+        }
+        else {
+            console.log("You can't answer before starting the game")
         }
         playerInputElement.value = ''
     }
 }
 // --- event listener --- //
 StartButtonElement.addEventListener('click', startFunc)
+ExitButtonElement.addEventListener('click', resetLevel)
 
 // document.addEventListener('keydown', (event) => {
 //     console.log(event)
@@ -150,10 +158,14 @@ StartButtonElement.addEventListener('click', startFunc)
 document.addEventListener('keydown', checkEnter)
 
 
-// how to make showing less than 3 words also randomized
-// when user press start, he can't press it again, also add an exit button, so when the user press exit it exit the main menu
-// when I press enter, I want the input to be empty again, so i don't have to delete my previous answer.
-// I don't want the same question to repeat again.
-// add a level indicator as p in html to display text content.
+// how to make showing less than 3 words also randomized. done
+// add a level indicator as p in html to display text content. done
+// when I press enter, I want the input to be empty again, so i don't have to delete my previous answer. done
+// when user press start, he can't press it again, done
+// also add an exit button, so when the user press exit it exit the main menu. done
+// I don't want the same question to repeat again. done
+
+
+// add 
 
 
