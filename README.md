06a49ee3c7fc363a14ac13be34358dc5ddf50966
Ali <ali.kamel5958@gmail.com>
2026-08-19 18:08:21 +0300

    done, checking..

---
+++ README.md
@@ -0,0 +1,27 @@
+# Arabic Roots Game (لعبة جذور الكلمات)
+
+## Description
+A fast-paced word game built with JavaScript, HTML, and CSS. The goal of the game is to test your knowledge of Arabic word roots. Given a list of Arabic words, you must guess the correct root word before time runs out!
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## How to Play / User Stories
+1. Click the Start button to begin the timer and load the first question.
+2. Look at the group of words displayed on the screen.
+3. Type the root word in the input box and press Enter.
+4. Correct answers increase your score level and add bonus time to the countdown timer.
+5. If the timer reaches 0, the game ends. Click Start to reset the game to Level 0.
+
+## Future Enhancements
+- Add audio sound effects for correct/wrong answers.
+- Add a high score leaderboard using.
+- Add more 3-letters roots (there's 1000+ of them)
+- Add 4-letters and 5-letters roots for harder levels.
+- Add difficulty levels (Easy, Medium, Hard).
+
+## Credits
+Ali Kamel
+Built as a General Assembly Project.
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ----- Variables ----- //
-let time = 10
+let time = 15
 let currentTime = time
 let levelIndex = 0
 let level = 0
@@ -59,6 +59,354 @@ const questions = [
         words: ["خباز", "مخبز", "خبزة"],
         Root: "خبز"
     },
+    {
+        words: ["مزرعة", "زراعة", "مزروع"],
+        Root: "زرع"
+    },
+    {
+        words: ["تنظيف", "نظافة", "منظف"],
+        Root: "نظف"
+    },
+    {
+        words: ["منظر", "نظارة", "منظار"],
+        Root: "نظر"
+    },
+    {
+        words: ["مسكن", "سكان", "إسكان"],
+        Root: "سكن"
+    },
+    {
+        words: ["طالب", "مطلوب", "مطالبة"],
+        Root: "طلب"
+    },
+    {
+        words: ["مجموعة", "جامعة", "اجتماع"],
+        Root: "جمع"
+    },
+    {
+        words: ["تفريق", "فرقة", "افتراق"],
+        Root: "فرق"
+    },
+    {
+        words: ["خروج", "مخرج", "إخراج"],
+        Root: "خرج"
+    },
+    {
+        words: ["دخول", "مدخل", "تدخل"],
+        Root: "دخل"
+    },
+    {
+        words: ["مصنع", "صناعة", "صانع"],
+        Root: "صنع"
+    },
+    {
+        words: ["محفظة", "حافظ", "محافظة"],
+        Root: "حفظ"
+    },
+    {
+        words: ["شاكر", "مشكور", "شكران"],
+        Root: "شكر"
+    },
+    {
+        words: ["رحمة", "رحيم", "مرحوم"],
+        Root: "رحم"
+    },
+    {
+        words: ["غفران", "مغفرة", "استغفار"],
+        Root: "غفر"
+    },
+    {
+        words: ["عبادة", "معبد", "عابد"],
+        Root: "عبد"
+    },
+    {
+        words: ["سجود", "مسجد", "ساجد"],
+        Root: "سجد"
+    },
+    {
+        words: ["مشغول", "انشغال", "أشغال"],
+        Root: "شغل"
+    },
+    {
+        words: ["سؤال", "مسؤول", "تساؤل"],
+        Root: "سأل"
+    },
+    {
+        words: ["قراءة", "قارئ", "مقروء"],
+        Root: "قرأ"
+    },
+    {
+        words: ["بداية", "مبتدئ", "ابتداء"],
+        Root: "بدأ"
+    },
+    {
+        words: ["هدوء", "تهدئة", "هادئ"],
+        Root: "هدأ"
+    },
+    {
+        words: ["نشأة", "إنشاء", "ناشئ"],
+        Root: "نشأ"
+    },
+    {
+        words: ["أمان", "مؤمن", "إيمان"],
+        Root: "أمن"
+    },
+    {
+        words: ["أمير", "إمارة", "مؤتمر"],
+        Root: "أمر"
+    },
+    {
+        words: ["مؤذن", "أذان", "استئذان"],
+        Root: "أذن"
+    },
+    {
+        words: ["مأكل", "أكلة", "مأكولات"],
+        Root: "أكل"
+    },
+    {
+        words: ["تأسيس", "مؤسسة", "أساس"],
+        Root: "أسس"
+    },
+    {
+        words: ["تأثير", "مؤثر", "آثار"],
+        Root: "أثر"
+    },
+    {
+        words: ["أديب", "تأديب", "آداب"],
+        Root: "أدب"
+    },
+    {
+        words: ["آمال", "مأمول", "تأمل"],
+        Root: "أمل"
+    },
+    {
+        words: ["أليم", "مؤلم", "آلام"],
+        Root: "ألم"
+    },
+    {
+        words: ["تأخير", "متأخر", "آخرة"],
+        Root: "أخر"
+    },
+    {
+        words: ["رئيس", "رئاسة", "مرؤوس"],
+        Root: "رأس"
+    },
+    {
+        words: ["جزء", "تجزئة", "أجزاء"],
+        Root: "جزأ"
+    },
+    {
+        words: ["امتلاء", "مملوء", "ممتلئ"],
+        Root: "ملأ"
+    },
+    {
+        words: ["براءة", "بريء", "تبرئة"],
+        Root: "برأ"
+    },
+    {
+        words: ["دفء", "تدفئة", "دافئ"],
+        Root: "دفأ"
+    },
+    {
+        words: ["لاجئ", "ملجأ", "التجاء"],
+        Root: "لجأ"
+    },
+    {
+        words: ["مفاجأة", "فجأة", "مفاجئ"],
+        Root: "فجأ"
+    },
+    {
+        words: ["تهنئة", "هنيء", "تهاني"],
+        Root: "هنأ"
+    },
+    {
+        words: ["موعد", "ميعاد", "وعيد"],
+        Root: "وعد"
+    },
+    {
+        words: ["مواصلات", "وصلة", "اتصال"],
+        Root: "وصل"
+    },
+    {
+        words: ["موقف", "توقف", "موقوف"],
+        Root: "وقف"
+    },
+    {
+        words: ["ميلاد", "مولود", "ولادة"],
+        Root: "ولد"
+    },
+    {
+        words: ["ميزان", "موازنة", "وزنة"],
+        Root: "وزن"
+    },
+    {
+        words: ["وجود", "موجود", "إيجاد"],
+        Root: "وجد"
+    },
+    {
+        words: ["واجهة", "مواجهة", "اتجاه"],
+        Root: "وجه"
+    },
+    {
+        words: ["مقولة", "أقوال", "قائل"],
+        Root: "قول"
+    },
+    {
+        words: ["بائع", "مبيع", "مبايعة"],
+        Root: "بيع"
+    },
+    {
+        words: ["منام", "نائم", "تنويم"],
+        Root: "نوم"
+    },
+    {
+        words: ["صيام", "صائم", "يصوم"],
+        Root: "صوم"
+    },
+    {
+        words: ["مقام", "إقامة", "استقامة"],
+        Root: "قوم"
+    },
+    {
+        words: ["زيارة", "زائر", "مزار"],
+        Root: "زور"
+    },
+    {
+        words: ["مسيرة", "سيارة", "مسير"],
+        Root: "سير"
+    },
+    {
+        words: ["طيران", "طائر", "مطار"],
+        Root: "طير"
+    },
+    {
+        words: ["معيشة", "عيشة", "معاش"],
+        Root: "عيش"
+    },
+    {
+        words: ["خائف", "مخيف", "تخويف"],
+        Root: "خوف"
+    },
+    {
+        words: ["دعاء", "داعية", "دعوة"],
+        Root: "دعو"
+    },
+    {
+        words: ["رماية", "مرمى", "رامي"],
+        Root: "رمي"
+    },
+    {
+        words: ["ماشي", "ممشى", "مشية"],
+        Root: "مشي"
+    },
+    {
+        words: ["ساعي", "مسعى", "مساعي"],
+        Root: "سعي"
+    },
+    {
+        words: ["بناء", "بناية", "مبنى"],
+        Root: "بني"
+    },
+    {
+        words: ["جريان", "مجرى", "إجراء"],
+        Root: "جري"
+    },
+    {
+        words: ["قضاء", "قاضي", "قضية"],
+        Root: "قضي"
+    },
+    {
+        words: ["لقاء", "ملتقى", "تلاقي"],
+        Root: "لقي"
+    },
+    {
+        words: ["نسيان", "منسي", "ناسي"],
+        Root: "نسي"
+    },
+    {
+        words: ["حكاية", "حاكي", "محكي"],
+        Root: "حكي"
+    },
+    {
+        words: ["محبة", "حبيب", "أحباب"],
+        Root: "حبب"
+    },
+    {
+        words: ["امتداد", "مدة", "ممدود"],
+        Root: "مدد"
+    },
+    {
+        words: ["مردود", "ترديد", "ارتداد"],
+        Root: "ردد"
+    },
+    {
+        words: ["شديد", "تشديد", "اشتاد"],
+        Root: "شدد"
+    },
+    {
+        words: ["حاج", "حجة", "محجة"],
+        Root: "حجج"
+    },
+    {
+        words: ["سرور", "مسرة", "سريرة"],
+        Root: "سرر"
+    },
+    {
+        words: ["حلال", "محل", "تحليل"],
+        Root: "حلل"
+    },
+    {
+        words: ["مرور", "ممر", "استمرار"],
+        Root: "مرر"
+    },
+    {
+        words: ["صحة", "تصحيح", "صحيح"],
+        Root: "صحح"
+    },
+    {
+        words: ["خاص", "تخصص", "اختصاص"],
+        Root: "خصص"
+    },
+    {
+        words: ["حريق", "محرقة", "إحراق"],
+        Root: "حرق"
+    },
+    {
+        words: ["مطبعة", "طباعة", "انطباع"],
+        Root: "طبع"
+    },
+    {
+        words: ["رسام", "مرسم", "رسومات"],
+        Root: "رسم"
+    },
+    {
+        words: ["سباحة", "مسبح", "سابح"],
+        Root: "سبح"
+    },
+    {
+        words: ["مركبة", "راكب", "تركيب"],
+        Root: "ركب"
+    },
+    {
+        words: ["مسافر", "سفارة", "أسفار"],
+        Root: "سفر"
+    },
+    {
+        words: ["ناقلة", "انتقال", "منقول"],
+        Root: "نقل"
+    },
+    {
+        words: ["حمال", "محمول", "حملة"],
+        Root: "حمل"
+    },
+    {
+        words: ["مرتفع", "ارتفاع", "رافعة"],
+        Root: "رفع"
+    },
+    {
+        words: ["منزل", "نزول", "تنزيل"],
+        Root: "نزل"
+    }
 ]
 // --- cache --- //
 const StartButtonElement = document.querySelector('#start-button')
@@ -112,10 +460,15 @@ function leveling() {
         questions[levelIndex].words.sort(() => Math.random() - .5)
         console.log(questions[levelIndex].words[0] + " | " + questions[levelIndex].words[1])
         questionWordsElement.textContent = questions[levelIndex].words[0] + " | " + questions[levelIndex].words[1]
-    } else if (level >= 21) {
+    } else if (level >= 21 && level <= 100) {
         questions[levelIndex].words.sort(() => Math.random() - .5)
         console.log(questions[levelIndex].words[0])
         questionWordsElement.textContent = questions[levelIndex].words[0]
+    }
+        else if (level >=100){
+        console.log("You Won !")
+        questionWordsElement.textContent = "You Won !"    
+        
     }
 }
 function resetLevel() {
@@ -131,12 +484,22 @@ function checkEnter(event) {
     if (event.code == 'Enter') {
         console.log("You just pressed Enter")
         console.log(playerInputElement.value)
+        console.log(questions[levelIndex].Root)
         if (playerInputElement.value === questions[levelIndex].Root && active == true) {
 
             console.log("Your Answer is True")
-            currentTime += 2
+        
             level = level + 1
             levelIndex++
+            if (level >= 0 && level <= 31) {
+            currentTime += 3
+            }
+            else if(level >= 31 && level <= 80) {
+            currentTime += 2
+            }
+            else if(level >= 80) {
+            currentTime += 2
+            }
             leveling()
         }
         else if (playerInputElement.value != questions[levelIndex].Root && active == true) {
@@ -166,6 +529,6 @@ document.addEventListener('keydown', checkEnter)
 // I don't want the same question to repeat again. done
 
 
-// add 
+// add more questions. done
 
 
